c7188d9d35ea67169e4dc18111300ca11c2131d4
miguelmorais7 <42868106+miguelmorais7@users.noreply.github.com>
2021-03-30 10:51:36 -0300

    CRUD/Routes finished

--- server/src/controllers/employees.controller.js
+++ server/src/controllers/employees.controller.js
@@ -3,27 +3,36 @@ const employeeController = {}
 
 const Employee = require('../models/Employee');
 
+//GET
 employeeController.getEmployees = async (req,res) => {
-    const employees = await Employee.find()
-    res.json(employees)
+    const employees = await Employee.find();
+    res.json(employees);
 }
 
-employeeController.getEmployee = (req,res) => {
-    res.send('get employee');
+employeeController.getEmployee = async (req,res) => {
+    //Employee.findOne({_id: req.params.id})
+    const employee = await Employee.findById(req.params.id);
+    res.send(employee);
 }
 
+//POST
 employeeController.createEmployee = async (req,res) => {
     const newEmployee = new Employee(req.body);
-    await newEmployee.save()
+    //console.log(newEmployee);
+    await newEmployee.save();
     res.send({ message: "Employee created" });
 }
 
-employeeController.updateEmployee = (req,res) => {
-    res.send('update employee');
+//PUT
+employeeController.updateEmployee = async (req,res) => {
+    await Employee.findByIdAndUpdate(req.params.id, req.body);
+    res.json({status: "Employee updated"});
 }
 
-employeeController.deleteEmployee = (req,res) => {
-    res.send('delete employee');
+//DELETE
+employeeController.deleteEmployee = async (req,res) => {
+    await Employee.findByIdAndDelete(req.params.id)
+    res.json({status: 'Employee deleted'});
 }
 
 module.exports = employeeController;
